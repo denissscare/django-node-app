3fad5e560454e30f2b3134d0dc43e3382b801ddb
Denis <denissscare@gmail.com>
2024-11-10 14:41:04 +0800

    add drag&drop admin sort

--- spaceAgency/photos/admin.py
+++ spaceAgency/photos/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from .models import Photo
+from adminsortable2.admin import SortableAdminMixin
 
-admin.site.register(Photo)
+
+class PhotoAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ('title', 'image_preview')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="250px" height="250px" style="object-fit: cover;" />')
+        return '-'
+    image_preview.allow_tags = True  # Разрешаем вывод HTML
+
+admin.site.register(Photo, PhotoAdmin)
--- spaceAgency/photos/models.py
+++ spaceAgency/photos/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+
 class Photo(models.Model):
   title = models.CharField(max_length=100)
   image = models.ImageField(upload_to='photos/')
+  my_order = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+    )
   
   def __str__(self):
-    return self.title 
+    return self.title
+  
+  class Meta:
+        ordering = ['my_order']
